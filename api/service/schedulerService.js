47488d7a0782d29c1adac86b3c114141c731125b
jb087 <jb087@op.pl>
2020-04-30 11:57:33 +0200

    # Changed scheduler engine

--- api/service/schedulerService.js
+++ api/service/schedulerService.js
@@ -1,32 +1,31 @@
 const express = require('express');
 const fetch = require('node-fetch');
-const CronJob = require('cron').CronJob;
+const schedule = require('node-schedule');
 
 const router = express.Router();
 
 const host = "http://localhost:9000/";
 const getRemindersPath = host + "api/reminders";
 
-const everyTwoSecond = "0/2 * * * * *";
+const everyTwoSecond = "*/2 * * * * *";
 
 let jobs = new Map();
 
-new CronJob(everyTwoSecond, function () {
+schedule.scheduleJob(everyTwoSecond, function () {
     fetch(getRemindersPath, {
         method: "GET",
         headers: {"Content-Type": "application/json"}
     })
         .then(response => response.json())
         .then(reminders => runReminders(reminders));
-}, null, true, 'Europe/Warsaw').start();
+});
 
 function runReminders(reminders) {
     reminders.forEach(reminder => {
         if (jobs.get(reminder.id) === undefined) {
-            let job = new CronJob(reminder.time, function () {
+            let job = schedule.scheduleJob(reminder.time, function () {
                 console.log("JobId: " + reminder.id)
-            }, null, true, 'Europe/Warsaw');
-            job.start();
+            });
 
             jobs.set(reminder.id, job);
         }
