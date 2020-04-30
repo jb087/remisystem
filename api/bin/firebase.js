e8d3782db8a5ebeefa3804c98fc948e005b27971
jb087 <jb087@op.pl>
2020-04-30 15:33:47 +0200

    # Added firebase config # Added firebase admin

--- api/bin/firebase.js
+++ api/bin/firebase.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCuABB6prPSiXSsxI7pYnZ1DpnGHT40uzU",
-    authDomain: "remisystem-63655.firebaseapp.com",
-    databaseURL: "https://remisystem-63655.firebaseio.com",
-    projectId: "remisystem-63655",
-    storageBucket: "remisystem-63655.appspot.com",
-    messagingSenderId: "208731283939",
-    appId: "1:208731283939:web:cf4cc6aa36fd92816180d6",
-    measurementId: "G-CNJZFJ1HCF"
+    apiKey: "AIzaSyAGnWAYBQjnjuEbemUMAtI-HPNbqpjJVQ0",
+    authDomain: "remisystem-75ab6.firebaseapp.com",
+    databaseURL: "https://remisystem-75ab6.firebaseio.com",
+    projectId: "remisystem-75ab6",
+    storageBucket: "remisystem-75ab6.appspot.com",
+    messagingSenderId: "506713044142",
+    appId: "1:506713044142:web:28aabdc3c648653976293f",
+    measurementId: "G-P7P7DK6T86"
 };
