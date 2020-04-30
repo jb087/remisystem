a770cfd572bf1b33f74513977e1e1b6df47c314d
jb087 <jb087@op.pl>
2020-04-30 15:14:51 +0200

    # Added firebase config

---
+++ api/bin/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCuABB6prPSiXSsxI7pYnZ1DpnGHT40uzU",
+    authDomain: "remisystem-63655.firebaseapp.com",
+    databaseURL: "https://remisystem-63655.firebaseio.com",
+    projectId: "remisystem-63655",
+    storageBucket: "remisystem-63655.appspot.com",
+    messagingSenderId: "208731283939",
+    appId: "1:208731283939:web:cf4cc6aa36fd92816180d6",
+    measurementId: "G-CNJZFJ1HCF"
+};
