1329ba7efbfd699315e0f5002ac9d43940c97d3e
jb087 <jb087@op.pl>
2020-05-03 10:34:37 +0200

    # Added possibility to enable/disable reminders on mail

--- api/service/mailService.js
+++ api/service/mailService.js
@@ -1,6 +1,10 @@
 const nodemailer = require('nodemailer');
 const admin = require("firebase-admin");
+const firestore = admin.firestore();
 require('dotenv').config();
+
+const userSettingCollection = 'userSettings';
+const sendEmailRemindersProperty = 'sendEmailReminders';
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -13,19 +17,36 @@ const transporter = nodemailer.createTransport({
 exports.sendReminderOnMail = (note) => {
     admin.auth().getUser(note.userId)
         .then(user => {
-            let mailOptions = getMailOptions(note, user.email);
-            transporter.sendMail(mailOptions, function (error, info) {
-                if (error) {
-                    console.log(error);
-                } else {
-                    console.log('Email sent: ' + info.response);
-                }
-            });
+            sendReminderOnMail(note, user);
         })
         .catch(function(error) {
             console.log('Error fetching user data: ', error);
         });
 };
+
+function sendReminderOnMail(note, user) {
+    firestore.collection(userSettingCollection)
+        .doc(user.uid)
+        .get()
+        .then(snapshot => {
+            if (snapshot.get(sendEmailRemindersProperty)) {
+                sendMail(note, user);
+            } else {
+                console.log('Unable to send email due to disable this functionality.')
+            }
+        })
+}
+
+function sendMail(note, user) {
+    let mailOptions = getMailOptions(note, user.email);
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}
 
 function getMailOptions(note, userMail) {
     return {
