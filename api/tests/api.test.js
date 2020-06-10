7f9a5046d9f64ecdedea360534245f1414abde13
Ernei3 <45853073+Ernei3@users.noreply.github.com>
2020-06-10 14:05:14 +0200

    Test - check get note by id.

--- api/tests/api.test.js
+++ api/tests/api.test.js
@@ -21,6 +21,7 @@ describe('GET /api/notes', () => {
     });
     it('When there is an authorized request, the server returns all notes.', async () => {
         // Act:
+        const noteDescription = "test";
         const result = await request.get('/api/notes')
             .set('Authorization', `Bearer ${accessToken}`);
 
@@ -28,5 +29,26 @@ describe('GET /api/notes', () => {
         expect(result.status).toBe(200);
         expect(result.body.length).toBeGreaterThanOrEqual(1);
         expect(result.body.some(note => note.description === noteDescription)).toBe(true);
+        console.log(result.body)
+    });
+});
+
+describe('GET /api/note/:id', () => {
+    it('When there is an unauthorized request the server returns 401.', async () => {
+        // Act:
+        const result = await request.get('/api/note/:id');
+        // Result
+        expect(result.status).toBe(401);
+    });
+    it('When there is an authorized request, the server returns the note with the given id.', async () => {
+        // Act:
+        const noteId = "541ac530-8d1d-11ea-8451-2d64d0a1f6ef";
+        const noteDescription = "test";
+        const result = await request.get(`/api/note/${noteId}`)
+            .set('Authorization', `Bearer ${accessToken}`);
+
+        // Result
+        expect(result.status).toBe(200);
+        expect(result.body.description === noteDescription).toBe(true);
     });
 });
